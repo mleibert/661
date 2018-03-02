38234860d5cc66f1f18b5fcd8c224db6d9103989
mleibert <mike.leibert@gmail.com>
2018-03-02 12:45:59 -0500

    1

--- beetles.R
+++ beetles.R
@@ -18,21 +18,65 @@ crabs$color<-as.factor(CRABS$color)
 crabs$y<-ifelse(crabs$satellite > 0, 1,0)
 crabs<-crabs[,c(2,4,ncol(crabs))] 
 crabs$color<-as.factor(crabs$color)
+crabs$color<-factor(crabs$color,levels(crabs$color)[c(4,1,2,3)])
 
 head(crabs)
 str(crabs)
 
 #main effect & interaction model
 crab.glm<-glm( y~.,data=crabs ,family="binomial" )
-crabI.glm<-glm( y~weight*color,data=crabs ,family="binomial" )
-
-
+glm( y~weight+factor(color),data=crabs ,family="binomial" )
 summary(crab.glm) 
+crabby.glm<-glm(y~weight,data=crabs ,family="binomial" )
+summary(crabby.glm) 
+
+
+#Wald tests
+#Just for H0:b1 = 0
+sqrt ( diag( vcov(crab.glm) ) )[2]
+
+coef(crab.glm)[2] /   sqrt( diag( vcov(crab.glm) ) )[2] 
+
+2*(1-pnorm(4.353582))
+
+qnorm(0.975)
+
+ anova(crab.glm, test="Chisq")
+
+#wald CI for b1
+coef(crab.glm)[2] - qnorm(.975)*sqrt(diag(vcov(crab.glm)))[2]
+coef(crab.glm)[2] + qnorm(.975)*sqrt(diag(vcov(crab.glm)))[2]
+
+confint.default(crab.glm)
+
+
+wald.test(b= coef(crab.glm)[2] , sqrt ( diag( vcov(crab.glm) ) )[2],Terms=1)
+
+
+#To test the significance of color, controlling for weight we must test 
+#H0 : ß2 = ß3 = ß4 = 0. The likelihood-ratio statistic is
+#G^2 = -2(L0 - L1) with 3 degrees of freedom
+
+-2 * ( logLik(crabby.glm) - logLik(crab.glm) )
+
+ ( 195.7371- 188.54225   )
+
+1-pchisq(7.194895 , 3)
+
+summary(crabby.glm) 
+summary(crab.glm) 
+
+195.73715 - 188.54225
+
+crabI.glm<-glm( y~weight*color,data=crabs ,family="binomial" )
+vcov(crab.glm)
+
 #AIC: 
 summary(crab.glm)[[4]]
 #residual deviance
 summary(crab.glm)[[5]]	
 
+confint(crab.glm) 
 
 summary(crab.glm)		#nested model
 summary(crabI.glm)	#large model
