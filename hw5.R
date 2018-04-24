9f86a17cc771a0089aa6685aff353ba3a387287c
mleibert <mike.leibert@gmail.com>
2018-04-23 23:35:50 -0400

    2

--- hw5.R
+++ hw5.R
@@ -17,9 +17,6 @@ names(smoke)<-c(paste0("PY",c("nonsmokers", "smokers")),
 	paste0("CD",c("nonsmokers", "smokers")))
 
 
-smoke$age<-as.numeric(smoke$age)
-smoke$age<-as.factor(smoke$age)
-
 smokers<-data.frame(rep(1:5,2),c(rep("NS",5),rep("S",5))	,
 	c(smoke$PYnonsmokers,smoke$PYsmokers),
 	c(smoke$CDnonsmokers,smoke$CDsmokers))
@@ -27,16 +24,19 @@ smokers<-data.frame(rep(1:5,2),c(rep("NS",5),rep("S",5))	,
 names(smokers)<-c("age","smoker","PersonYears","Deaths")
 smokers$age<-as.factor(smokers$age)
 smokers$ageQI<-as.numeric(smokers$age)
+smokers$rates<-smokers$Deaths/smokers$PersonYears
 
 str(smokers)
+
+
 
 #1a
 smokers.fit<- glm(Deaths ~ age+smoker, offset = log(PersonYears),
 	family=poisson, data=smokers) 
  
-plot(smokers.fit$linear.predictors[1:5]  ,  ylim=c(1.5,6) )
-points(smokers.fit$linear.predictors[6:10])
-
+plot( (smokers.fit$linear.predictors[1:5])  ,  ylim=c(1.5,6) )
+points((smokers.fit$linear.predictors[6:10]))
+ 
 summary(smokers.fit)
 
 1-pchisq(12.13, 7)
@@ -47,6 +47,7 @@ summary(smokers.fit)
 smokersQI.fit<- glm(Deaths ~ ageQI*smoker, offset = log(PersonYears),
 	family=poisson, data=smokers) 
 
+summary(smokersQI.fit)
  1-pchisq(59.9 , 6)
 
 
@@ -82,8 +83,32 @@ data.frame(dat$response[1:19],
 
 tab<-cbind(dat[which(dat$gender == "F" ),],dat[which(dat$gender == "M" ),-1])
 tab<-tab[,c(1,2,4)]
+
+
+tab[19,2]<-sum(tab[19:nrow(tab),2])
+tab[19,3]<-sum(tab[19:nrow(tab),3])
+tab<-tab[1:19,]
 tab 
 names(tab)[2:3]<-c("Female","Male")
 
-sum(dat[which(dat$gender == "F" & dat$response > 19),2])
-sum(dat[which(dat$gender == "M" & dat$response > 19),2])
+
+sum(tab[,2])
+tab$PestF<-round(dpois(tab$response ,exp(1.45936 ))*310,2)
+tab$PestM<-round(dpois(tab$response ,exp(1.45936+ 0.30850 ))*sum(tab[,3]),2)
+
+
+nb.fit<-glm.nb(response ~ gender, weights=counts, data=dat)
+summary(nb.fit)
+summary(dat.fit)
+
+tab$PestF<-round(,2)
+tab$PestM<-round(dpois(tab$response ,exp(1.45936+ 0.30850 ))*sum(tab[,3]),2)
+
+
+muhat = unique(nb.fit$fitted.values)
+dnbinom(tab$response,size = nb.fit$theta, mu = muhat[2]) *310
+
+
+
+
+
